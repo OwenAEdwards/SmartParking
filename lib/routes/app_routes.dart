7dd2f5fdcf9027c0c62b7bc45d9248c6cc1b7ee0
Owen Edwards <edwardoa@mail.uc.edu>
2025-02-07 14:31:29 -0500

    Added home_screen

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking/views/home_screen.dart';
+import 'package:smart_parking/views/parking_screen.dart';
+
+class AppRoutes {
+  static Map<String, WidgetBuilder> routes = {
+    '/': (context) => const HomeScreen(),
+    // '/parking': (context) => const ParkingScreen(),
+  };
+}
