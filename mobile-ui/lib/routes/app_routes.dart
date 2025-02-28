8b9a46604b69c4b6107413f384124697aefe5b8a
Owen Edwards <edwardoa@mail.uc.edu>
2025-02-28 09:16:17 -0500

    Logging in now redirects to parking_screen

--- mobile-ui/lib/routes/app_routes.dart
+++ mobile-ui/lib/routes/app_routes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smart_parking/views/home_screen.dart';
 import 'package:smart_parking/views/parking_screen.dart';
 import 'package:smart_parking/views/login_screen.dart';
 import 'package:smart_parking/providers/auth_provider.dart';
@@ -10,7 +9,7 @@ class AppRoutes {
     '/': (context) {
       var isAuthenticated =
           Provider.of<AuthProvider>(context, listen: false).isAuthenticated;
-      return isAuthenticated ? const HomeScreen() : const LoginScreen();
+      return isAuthenticated ? const ParkingScreen() : const LoginScreen();
     },
     '/parking': (context) => const ParkingScreen(),
     '/login': (context) => const LoginScreen(),
