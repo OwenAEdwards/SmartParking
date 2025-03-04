9416d2dc51363cbe0f83cf7eda7aa6826af1c083
Owen Edwards <edwardoa@mail.uc.edu>
2025-03-04 12:04:09 -0500

    Frontend and backend talk now and the backend talks with Mongo

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -1,6 +1,13 @@
-﻿namespace SmartParking.Models;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class UserModel
 {
-    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Email { get; set; }
+    public string DisplayName { get; set; }
+    public string GoogleId { get; set; } // Store OAuth provider ID
 }
